3b24a3ec61475695def3bcc378ea3123c614f9ec
david02653 <david02653@gmail.com>
2021-11-15 02:41:15 +0800

    add english pipeline

--- en/actions/actions.py
+++ en/actions/actions.py
@@ -25,3 +25,89 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.interfaces import Action
+
+class ActionGreet(Action):
+    def name(self) -> Text:
+        return "action_greet"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            msg = {
+                "intent": "greet"
+            }
+            dispatcher.utter_message(format(msg))
+            return []
+
+class ActionSuggestionPass(Action):
+    def name(self) -> Text:
+        return "action_suggest_review_pass"
+    
+    def run(self, dispatcher: "CollectingDispatcher", 
+            tracker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            msg = {
+                "intent": "suggest_review_pass",
+                "reviewResult": True,
+                "endOfChat": True
+            }
+            dispatcher.utter_message(format(msg))
+            return []
+
+class ActionSuggestionFail(Action):
+    def name(self) -> Text:
+        return "action_suggest_review_fail"
+    def run(self, dispatcher: "CollectingDispatcher", 
+        tracker: Tracker, 
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        msg = {
+            "intent": "suggest_review_fail",
+            "reviewResult": False,
+            "endOfChat": True
+        }
+        dispatcher.utter_message(format(msg))
+        return []
+
+class ActionPersonalScoreQuery(Action):
+    def name(self) -> Text:
+        return "action_personal_score_query"
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        target = next(tracker.get_latest_entity_values("score_query_target"), None)
+        msg = {
+            "intent": "personal_score_query",
+            "target": target,
+            "endOfChat": True
+        }
+        dispatcher.utter_message(format(msg))
+        return []
+
+class ActionClassmapSearch(Action):
+    def name(self) -> Text:
+        return "action_classmap_search"
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        keyword = next(tracker.get_latest_entity_values("search_target"), None)
+        msg = {
+            "intent": "classmap_search",
+            "target": keyword,
+            "endOfChat": True
+        }
+        dispatcher.utter_message(format(msg))
+        return []
+
+class ActionClassmapPpt(Action):
+    def name(self) -> Text:
+        return "action_classmap_ppt"
+    def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        target = next(tracker.get_latest_entity_values("ppt_query_target"), None)
+        msg = {
+            "intent": "classmap_ppt",
+            "target": target,
+            "endOfChat": True
+        }
+        dispatcher.utter_message(format(msg))
+        return []
