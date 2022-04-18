c4ec46f5f3df999ee2a497cc64f5fb38f718a443
sheng-kai-wang <76858274+sheng-kai-wang@users.noreply.github.com>
2022-04-18 15:33:29 +0800

    removed personal_textbook_query feature

--- ch/actions/actions.py
+++ ch/actions/actions.py
@@ -32,42 +32,47 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.interfaces import Action
 
+
 class ActionGreet(Action):
     def name(self) -> Text:
         return "action_greet"
+
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            msg = {
-                "intent": "greet",
-                "endOfChat": True
-            }
-            # dispatcher.utter_message(format(msg))
-            dispatcher.utter_custom_json(format(msg))
-            return []
+        msg = {
+            "intent": "greet",
+            "endOfChat": True
+        }
+        # dispatcher.utter_message(format(msg))
+        dispatcher.utter_custom_json(format(msg))
+        return []
+
 
 class ActionSuggestionPass(Action):
     def name(self) -> Text:
         return "action_suggest_review_pass"
-    
-    def run(self, dispatcher: "CollectingDispatcher", 
-            tracker: Tracker, 
+
+    def run(self, dispatcher: "CollectingDispatcher",
+            tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            msg = {
-                "intent": "suggest_review_pass",
-                "reviewResult": True,
-                "endOfChat": True
-            }
-            # dispatcher.utter_message(format(msg))
-            dispatcher.utter_custom_json(format(msg))
-            return []
+        msg = {
+            "intent": "suggest_review_pass",
+            "reviewResult": True,
+            "endOfChat": True
+        }
+        # dispatcher.utter_message(format(msg))
+        dispatcher.utter_custom_json(format(msg))
+        return []
+
 
 class ActionSuggestionFail(Action):
     def name(self) -> Text:
         return "action_suggest_review_fail"
-    def run(self, dispatcher: "CollectingDispatcher", 
-        tracker: Tracker, 
-        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+    def run(self, dispatcher: "CollectingDispatcher",
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         msg = {
             "intent": "suggest_review_fail",
             "reviewResult": False,
@@ -77,11 +82,14 @@ class ActionSuggestionFail(Action):
         dispatcher.utter_custom_json(format(msg))
         return []
 
+
 class ActionPersonalScoreQuery(Action):
     def name(self) -> Text:
         return "action_personal_score_query"
+
     def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        entity = next(tracker.get_latest_entity_values("score_query_target"), "None")
+        entity = next(tracker.get_latest_entity_values(
+            "score_query_target"), "None")
         msg = {
             "intent": "personal_score_query",
             "entity": entity,
@@ -91,11 +99,14 @@ class ActionPersonalScoreQuery(Action):
         dispatcher.utter_custom_json(format(msg))
         return []
 
+
 class ActionClassmapSearch(Action):
     def name(self) -> Text:
         return "action_classmap_search"
+
     def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        entity = next(tracker.get_latest_entity_values("search_target"), "None")
+        entity = next(tracker.get_latest_entity_values(
+            "search_target"), "None")
         msg = {
             "intent": "classmap_search",
             "entity": entity,
@@ -105,11 +116,14 @@ class ActionClassmapSearch(Action):
         dispatcher.utter_custom_json(format(msg))
         return []
 
+
 class ActionClassmapPpt(Action):
     def name(self) -> Text:
         return "action_classmap_ppt"
+
     def run(self, dispatcher: "CollectingDispatcher", tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        entity = next(tracker.get_latest_entity_values("ppt_query_target"), "None")
+        entity = next(tracker.get_latest_entity_values(
+            "ppt_query_target"), "None")
         msg = {
             "intent": "classmap_ppt",
             "entity": entity,
